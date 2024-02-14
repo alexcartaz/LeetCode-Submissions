eb64ae7dfbe9b2f35ac02d4034efdd674c1ae006
alexcartaz <alexander.t.carter@gmail.com>
2024-02-14 01:14:33 +0000

    Sync LeetCode submission - K Closest Points to Origin (javascript)

---
+++ my-folder/problems/k_closest_points_to_origin/solution.js
@@ -0,0 +1,38 @@
+
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {    
+    
+    let unordered = [];
+    let ans = [];
+    let distance, obj;
+    let index = 0;
+    points.map( ([x,y]) => {
+        distance = Math.sqrt(x**2 + y**2);
+        obj = {
+            'dist': distance,
+            'pair': [x,y]
+        }
+        unordered.push(obj);
+        index++;
+    });
+    //console.log(unordered)
+    const ordered = unordered.sort((a, b) => {
+        return a.dist - b.dist;
+    });
+    //console.log(ordered)
+
+    ordered.length = k;
+    //console.log(ordered)
+
+    ordered.map( obj => {
+        ans.push(obj.pair)
+    })
+
+    return ans;
+};
+
+
